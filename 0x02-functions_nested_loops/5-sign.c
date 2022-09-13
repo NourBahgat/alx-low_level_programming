c5afc9e833984eeb6b899f8ca72c3207dd4fbaa9
NourBahgat <nourbahgat2003@gmail.com>
2022-09-13 16:39:14 -0700

    5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _islower - Short description, single line
+ * @c: contains value to be compared
+ * Return: Always 0.
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else {
+_putchar(48);
+return (0);
+}
+_putchar('\n')
+}
