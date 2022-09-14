5d4168da447919db810b8ae4e4b7f4c650071dae
NourBahgat <nourbahgat2003@gmail.com>
2022-09-13 19:49:38 -0700

    11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,26 +9,16 @@
 void print_to_98(int n)
 
 {
-if (n <= 98)
+if (n < 98)
 {
-while (n <= 98)
-{
-if (n == 98)
-printf("%d\n", n);
-else
+for (n = n; n < 98; n++)
 printf("%d, ", n);
-n = n + 1;
+printf("%d\n", 98);
 }
-}
-else if (n > 98)
-{
-while (n >= 98)
-{
-if (n == 98)
-printf("%d\n", n);
 else
+{
+for (n = n; n > 98; n--)
 printf("%d, ", n);
-n = n - 1;
-}
+printf("%d\n", 98);
 }
 }
