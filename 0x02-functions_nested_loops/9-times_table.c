585af6c358d9a070a744afd8612a065d0b4b59df
NourBahgat <nourbahgat2003@gmail.com>
2022-09-13 19:11:08 -0700

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,6 +11,7 @@ void times_table(void)
 int x;
 int y;
 int z;
+ 
 for (x = 0; x >= 9; x++)
 {
 for (y = 0; y >= 9; y++)
@@ -21,13 +22,14 @@ if (z <= 9)
 _putchar ((z % 10) + '0');
 _putchar (',');
 _putchar (' ');
+_putchar (' ');
 }
 else
 {
-_putchar ((z / 10) + '0');
-_putchar ((z % 10) + '0');
 _putchar (',');
 _putchar (' ');
+_putchar ((z / 10) + '0');
+_putchar ((z % 10) + '0');
 }
 }
 _putchar ('\n');
