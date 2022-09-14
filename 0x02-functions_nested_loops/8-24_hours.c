ba32107a18bac22e4330bc523c66b0678ccf5aa7
NourBahgat <nourbahgat2003@gmail.com>
2022-09-13 17:53:45 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,6 +10,7 @@ void jack_bauer(void)
 {
 int i;
 int j;
+
 for (i = 0; i < 24; i++)
 {
 for (j = i + 1; j < 60; j++)
@@ -20,6 +21,10 @@ _putchar (':');
 _putchar ((j / 10) + '0');
 _putchar ((j % 10) + '0');
 _putchar ('\n');
+
 }
+
+
 }
+
 }
