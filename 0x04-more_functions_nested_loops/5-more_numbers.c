64795fa193695e67ac9982bed1cbc66fc64ba010
NourBahgat <nourbahgat2003@gmail.com>
2022-09-15 09:26:29 -0700

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,6 +3,7 @@
 /**
  * more_numbers - prints the numbers, from 0 to 14
  * followed by a new line
+ * Return: no return
  */
 
 void more_numbers(void)
@@ -12,11 +13,8 @@ for (i = 0; i < 10; i++)
 {
 for (n = 0; n <= 14; n++)
 {
-if (n > 9)
-{
 _putchar(n / 10 + '0');
 _putchar(n % 10 + '0');
-}
 }
 _putchar('\n');
 }
