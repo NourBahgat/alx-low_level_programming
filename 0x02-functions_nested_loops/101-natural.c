872ea5f96bcf7dc4ec07c3cf293ad2ac0fd7307c
NourBahgat <nourbahgat2003@gmail.com>
2022-09-14 19:42:47 -0700

    13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 /**
- * prints sum of multiples of 3 or 5 below 1024
+ * main - prints sum of multiples of 3 or 5 below 1024
  *
- * Return:void
+ * Return: always 0
 */
 
 int main(void)
