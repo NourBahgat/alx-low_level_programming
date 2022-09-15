ff377194001d2342719a04e2f826c96e6ae13457
NourBahgat <nourbahgat2003@gmail.com>
2022-09-15 07:34:43 -0700

    0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _isupper - determines upper case letters
+ * @c: letter to be tested
+ * Return: 0 if low, 1 if up
+ */
+
+int _isupper(int c)
+{
+for (c = 'A'; c < 'Z'; c++)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
