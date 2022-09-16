85194ea3bc86f6aea452054793e4725f87ceefc5
NourBahgat <nourbahgat2003@gmail.com>
2022-09-15 17:04:13 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,15 +17,19 @@ printf("%s", "Fizz");
 }
 else if (i % 5 == 0)
 {
-printf("%s","Buzz");
+printf("%s", "Buzz");
 }
 else if (i % 3 == 0 && i % 5 == 0)
 {
-printf("%s","FizzBuzz");
+printf("%s", "FizzBuzz");
 }
 else
 {
-printf("%d ", i);
+printf("%d", i);
+}
+if (x != 100)
+{
+printf(" ");
 }
 }
 putchar('\n');
