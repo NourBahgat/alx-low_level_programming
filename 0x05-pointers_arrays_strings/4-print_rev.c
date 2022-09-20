bd57a322405c2b2925588d23c0e739e79eba965d
NourBahgat <nourbahgat2003@gmail.com>
2022-09-19 18:23:46 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - print the reverse of the string
+ * @s: string
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+int i, count = 0;
+while (s[count])
+{
+if (s[count] == '\0')
+break;
+count++;
+}
+for (i = count; i >= 0; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
