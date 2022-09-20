48413840aa2cff94abe18b6b977eb6cdd2a11fe6
NourBahgat <nourbahgat2003@gmail.com>
2022-09-19 18:42:11 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse string
+ * @s: string to be reversed
+ * Return: no return
+ */
+
+void rev_string(char *s)
+{
+int temp, count, i;
+while (s[count])
+{
+if (count == '\0')
+break;
+count++
+}
+for (i = 0; i < count/2; i++)
+{
+temp = s[i];
+s[i] = s[count - 1 - i];
+s[count - 1 - i] = temp;
+}
+}
+      
