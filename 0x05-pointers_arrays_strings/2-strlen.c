58138ad8aeb1cc086adf53fdc12393640835c3d9
NourBahgat <nourbahgat2003@gmail.com>
2022-09-19 17:52:22 -0700

    2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+int length = strlen(*s);
+return (length);
+}
