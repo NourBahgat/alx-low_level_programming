35c4c949119a48085898f38f292b867b96324a11
NourBahgat <nourbahgat2003@gmail.com>
2022-09-19 19:27:52 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - print half string
+ * @str: string to be printed
+ * Return: no return
+ */
+
+void puts_half(char *str)
+{
+int count, i, j;
+while (str[count] != 0)
+{
+count++;
+}
+if (count % 2 == 0)
+{
+for (i = count / 2; i < count; i++)
+{
+_putchar(str[i]);
+}
+}
+else
+{
+for (j = (count - 1) / 2; j < count - 1; j++)
+{
+_putchar(str[j + 1]);
+}
+}
+_putchar('\n');
+}
