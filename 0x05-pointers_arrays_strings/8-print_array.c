4336fcb5e5f5dd75f78f5d04308de4ef54545c93
NourBahgat <nourbahgat2003@gmail.com>
2022-09-19 19:47:26 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - print number of elements
+ * @a: array
+ * @n: number of elements
+ * Return: no return
+ */
+
+void print_array(int *a, int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+if (i == 0)
+printf("%d", a[i]);
+else
+  printf(" ,%d", a[i]);
+}
+printf("\n");
+}
