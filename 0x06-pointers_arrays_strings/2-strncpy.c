e00e91147abcd36e4506424a1870d45ca3520117
NourBahgat <nourbahgat2003@gmail.com>
2022-09-22 00:05:49 -0700

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,10 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0; i < n && dest[i] != 0; i++)
+for (i = 0; i < n && src[i] != 0; i++)
 {
-src[i] = dest[i];
+dest[i] = src[i];
 }
-src[i] = '\0';
+dest[i] = '\0';
+return (dest);
 }
