008f69bf216e2064360fb7cef7996f8c64d6eeda
NourBahgat <nourbahgat2003@gmail.com>
2022-09-22 02:57:53 -0700

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalize words
+ * @s: string to be capitalized
+ * Return: return value of s
+ */
+
+char *cap_string(char *)
+{
+int i, j;
+char separator[] = " \t\n,;.!?\"(){}";
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[0] >= 'a' && s[0] <= 'z')
+{
+s[0] -= 32;
+}
+for (j = 0; separators[j] != '\0'; j++)
+{
+if ((s[i] == separators[j]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
+{
+s[i + 1] -= 32;
+}
+}
+}
+return (s);
+}
+	    
