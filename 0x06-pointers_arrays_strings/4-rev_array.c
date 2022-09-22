183f6412495c9108f0779186f675d49b5771aea7
NourBahgat <nourbahgat2003@gmail.com>
2022-09-22 01:11:03 -0700

    4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - concatenate two strings
+ * @a: array to be reversed
+ * @n:number of elements
+ * Return: no return
+ */
+
+void reverse_array(int *a, int n)
+{
+int i;
+int b[n];
+for (i = 0; i < n; i++)
+{
+b[n - 1 - i] = a[i];
+}
+for (i = 0; i < n; i++)
+{
+a[i] = b[i];
+}
+}
