0d91f8fab39c4f6bbe3cbea3cb21bad58e66e2cf
NourBahgat <nourbahgat2003@gmail.com>
2022-09-21 23:45:29 -0700

    1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,11 +14,11 @@ while (dest[i] != '\0')
 {
 i++;
 }
-for (j = 0; j <= n; j++)
+for (j = 0; j < n; src[j] != '\0'; j++)
 {
 dest[i] = src [j];
 i++;
 }
-*dest++ = '\0';
+dest[i] = '\0';
 return (dest);
 }
