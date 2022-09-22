3ea3671a04f7a8446eec894463202f86b2161806
NourBahgat <nourbahgat2003@gmail.com>
2022-09-22 00:46:49 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,9 +11,9 @@ int _strcmp(char *s1, char *s2)
 {
 int i;
 int cond;
-for (i = 0; s1[i] == s2[i] && s1[i] != '\0'; i++)
+for (i = 0; s2[i] != '\0' && s1[i] != '\0'; i++)
 {
-if (s1 > s2 || s2 > s1)
+if (s1[i] != s2[i])
 {
 cond = s1[i] - s2[i];
 }
