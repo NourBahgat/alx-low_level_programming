6622614b622d38cfd69c731617634f7763d67c06
NourBahgat <nourbahgat2003@gmail.com>
2022-09-30 16:27:48 -0700

    1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print file name followed by a new line
+ * @argc: count of arguments
+ * @argv: arguments passed to the executable
+ * Return: 0
+ */
+
+int main(int argc, char *argv[] __attribute((unused)))
+{
+printf("%d\n", argc);
+return(0);
+}
