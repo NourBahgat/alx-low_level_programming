c90f67a851734144070a35478e0e58dddb9690b4
NourBahgat <nourbahgat2003@gmail.com>
2022-09-30 16:31:34 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print file name followed by a new line
+ * main - print num of arguments followed by a new line
  * @argc: count of arguments
  * @argv: arguments passed to the executable
  * Return: 0
  */
 
-int main(int argc, char *argv[] __attribute((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-printf("%d\n", argc);
-return(0);
+printf("%d\n", argc - 1);
+return (0);
 }
