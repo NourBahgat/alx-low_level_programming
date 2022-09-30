2c3396a8b14c66d59b9bbbece01b6cd632e5a645
NourBahgat <nourbahgat2003@gmail.com>
2022-09-30 16:46:26 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print product of two numbers
+ * @argc: count of arguments
+ * @argv: arguments passed to the executable
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+int p;
+p = argv[1] x argv[2];
+if (argc == 2)
+{
+printf("%d\n", p);
+}
+else
+{
+printf("error\n");
+return (1);
+}
+return (0);
+}
