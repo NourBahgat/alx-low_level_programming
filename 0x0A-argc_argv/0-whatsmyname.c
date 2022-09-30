a244796d9a5044720eb729e69f676d49233c7ee0
NourBahgat <nourbahgat2003@gmail.com>
2022-09-30 16:20:40 -0700

    0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print file name followed by a new line
+ * @argc: count of arguments
+ * @argv: arguments passed to the executable
+ * Return: 0
+ */
+
+int main(int argc __attribute__ ((unused)), char* argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
