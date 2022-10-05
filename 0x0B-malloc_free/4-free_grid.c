369b63f016da2bc888388c88c1c993922f64df9e
NourBahgat <nourbahgat2003@gmail.com>
2022-10-04 18:19:53 -0700

    4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free 2d array
+ * @grid: array to be freed
+ * @height: height of the array
+ * Return: no return
+ */
+
+void free_grid(int **grid, int height)
+{
+if (grid != NULL && height != NULL)
+{
+while (height >= 0)
+{
+free(grid[height]);
+height--;
+}
+free(grid);
+}
+}
+	      
