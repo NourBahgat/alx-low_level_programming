5d603e304a52b97e792be3a8520e8583e43284e2
NourBahgat <nourbahgat2003@gmail.com>
2022-10-06 11:06:48 -0700

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#inlude <stdlib.h>
+
+/**
+ * *malloc_checked - Returns a pointer to the allocated memory
+ * @b: integer to be stored
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+void *sawsan;
+sawsan = malloc(b);
+if(sawsan == NULL)
+{
+exit(98);
+}
+return(sawsan);
+}
