ac823d88a5d81c7cc0d9306183b9b7784312c8dc
NourBahgat <nourbahgat2003@gmail.com>
2022-10-12 19:43:55 -0700

    2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,14 +13,14 @@ int i;
 if (cmp && array)
 {
 if (size <= 0)
-return -1;
+return (-1);
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]))
 {
-return i;
+return (i);
 }
 }
 }
-return -1;
+return (-1);
 }
